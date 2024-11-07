c3214f73e3a22daef22515234af2169697e03108
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-06 21:04:56 -0300

    fazendo o cronômetro do projeto funcionar

--- js/script.js
+++ js/script.js
@@ -3,10 +3,40 @@ const horasInput = document.querySelector(`#horas`)
 const minutosInput = document.querySelector(`#minutos`)
 const segundosInput = document.querySelector(`#segundos`)
 
-let dia;
-let horas;
-let minutos;
-let segundos;
+let dia = 2;
+let horas = 12;
+let minutos = 25;
+let segundos = 32;
 
-const data = new Date().toLocaleString(`pt-BR`)
-console.log(data);
+function criaDias(params) {
+    setInterval(()=>{
+        if (segundos === 0) return
+        segundos--
+    },1000000)
+}
+
+function criaHoras(params) {
+    setInterval(()=>{
+        if (horas === 0) return
+        segundos--
+    },100000)
+}
+
+function criaMinutos() {
+    setInterval(()=>{
+        if (minutos === 0) return
+        segundos--
+    },10000)
+}
+
+function criaSegundos(){
+    setInterval(()=>{
+        if (segundos === 0) return
+        segundos--
+    },1000)
+}
+
+
+
+// const data = new Date().toLocaleString(`pt-BR`)
+// console.log(data);
