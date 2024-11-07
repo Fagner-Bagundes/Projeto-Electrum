8c5951b529624d7dfd1a544e340faa1acafc132b
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-06 21:31:37 -0300

    tentando fazer o cronômetro funcionar

--- js/script.js
+++ js/script.js
@@ -3,40 +3,73 @@ const horasInput = document.querySelector(`#horas`)
 const minutosInput = document.querySelector(`#minutos`)
 const segundosInput = document.querySelector(`#segundos`)
 
-let dia = 2;
-let horas = 12;
-let minutos = 25;
-let segundos = 32;
+let dias = 2;
+let horas = 5;
+let minutos = 5;
+let segundos = 5;
+
+diaInput.innerHTML = dias;
+function adicionaCronometro(params) {
+    horasInput.innerHTML = horas;
+    minutosInput.innerHTML = minutos;
+    segundosInput.innerHTML = segundos;
+
+}
+
+
 
 function criaDias(params) {
-    setInterval(()=>{
-        if (segundos === 0) return
-        segundos--
-    },1000000)
+    let timerdias = setInterval(()=>{
+        --dias
+        console.log(dias);
+        
+        if (dias <= 0){
+            clearInterval(timerdias)
+        }
+        adicionaCronometro()
+    },1000)
 }
 
 function criaHoras(params) {
-    setInterval(()=>{
-        if (horas === 0) return
-        segundos--
-    },100000)
+    console.log(horas);
+    
+    let timerHr = setInterval(()=>{
+        --horas
+        adicionaCronometro()
+        if (horas <= 0){
+            clearInterval(timerHr)
+            criaDias();
+            return
+        }
+    },1000)
 }
 
 function criaMinutos() {
-    setInterval(()=>{
-        if (minutos === 0) return
-        segundos--
-    },10000)
+    let timerMin = setInterval(()=>{
+        --minutos
+        adicionaCronometro()
+        
+        if (minutos <= 0){
+            clearInterval(timerMin)
+            criaHoras()
+            return
+        }
+    },)
 }
 
 function criaSegundos(){
-    setInterval(()=>{
-        if (segundos === 0) return
-        segundos--
+    let timerSeg = setInterval(()=>{
+        console.log(segundos);
+        --segundos
+        adicionaCronometro()
+        
+        if (segundos <= 0) {
+            clearInterval(timerSeg)
+            criaMinutos()
+            return
+        }
     },1000)
 }
 
 
-
-// const data = new Date().toLocaleString(`pt-BR`)
-// console.log(data);
+criaSegundos();
